1e64136689dba0e63fd9f902056805d37db1ef00
diu <bongban2003@gmail.com>
2025-01-08 09:02:14 +0000

    10 fix

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,5 +11,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+	if (tree->parent == NULL)
+		return (0);
 	return (1 + binary_tree_depth(tree->parent));
 }
