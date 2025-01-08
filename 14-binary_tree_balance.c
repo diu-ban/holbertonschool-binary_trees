f637f0e7924a1bc0522a0b929835ece7158aa004
diu <bongban2003@gmail.com>
2025-01-08 10:07:03 +0000

    14 fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,9 +7,9 @@
  *
  * Return: Height of the tree. If tree is NULL, return 0.
  */
-int binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-        int lh, rh;
+        size_t lh, rh;
 
         if (tree == NULL)
                 return (0);
