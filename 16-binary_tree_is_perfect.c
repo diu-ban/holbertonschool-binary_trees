de38a45ad679247de8fd2e97a1872aa23e5859bd
diu <bongban2003@gmail.com>
2025-01-09 04:46:04 +0000

    fix 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -15,7 +15,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
     	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+		return (1);
 	left_depth = binary_tree_depth(tree->left);
 	right_depth = binary_tree_depth(tree->right);
 
